56824e914f6eff62786501fccf8a77982bac6393
ginkouno <gh.ginkouno@gmail.com>
2019-01-07 18:23:12 +0900

    Add main process

---
+++ remark-editor/main.js
@@ -0,0 +1,20 @@
+// app および BrowserWindowへの参照を読み込む
+const {app, BrowserWindow} = require('electron');
+
+// Windowを管理する変数
+let win;
+
+// メインウィンドウを作成するための関数
+function createWindow() {
+  win = new BrowserWindow({width:800, height: 600});
+  win.loadURL(`file://${__dirname}/mainwindow.html`);
+  win.on('closed', () => win = null; });  
+}
+
+// アプリケーションが準備できたらメインウィンドウを作成する
+app.on('ready', createWindow);
+
+// macOSではすべてのウィンドウが閉じた際にアプリケーションを終了させる
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
